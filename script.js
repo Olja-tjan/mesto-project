0716cc6f0af1a2ccd6008435a522e603135c8cda
Olga Bulgakova <olga.bulgakova.1997@mail.ru>
2023-06-12 16:38:08 +0700

    Ready 'Opened and close popup'

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
-let popup = document.querySelector('.popup');
-let buttonProfile = document.querySelector('.profile__edit-button')
-let buttonClose = document.querySelector('.popup__button-close')
+// Работа модальных окон
+
+const popup = document.querySelector('.popup');
+const buttonProfile = document.querySelector('.profile__edit-button');
+const buttonClose = document.querySelector('.popup__button-close');
+const profileName = document.querySelector('.profile__name');
+const profileDescription = document.querySelector('.profile__description');
+const formElement = document.querySelector('.popup__container-form');
+const nameInput = document.querySelector('#name');
+const jobInput = document.querySelector('#description');
 
 function openedPopup () {
-  popup.classList.add('popup_opened')
+  popup.classList.add('popup_opened');
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileDescription.textContent;
+};
+
+
+
+function closePopup () {
+  popup.classList.remove('popup_opened');
+};
+
+
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+
+  let profileName = document.querySelector('.profile__name');
+  let profileDescription = document.querySelector('.profile__description');
+
+  profileName.textContent = nameInput.value;
+  profileDescription.textContent = jobInput.value;
 };
 
 buttonProfile.addEventListener('click', openedPopup);
-
-function closePopup () {
-  popup.classList.remove('popup_opened')
-}
-
 buttonClose.addEventListener('click', closePopup);
+formElement.addEventListener('submit', handleFormSubmit);
+
