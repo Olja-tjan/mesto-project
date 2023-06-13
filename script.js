da135a6b00b5b1b16b39817eb7dd9c317d8cde85
Olga Bulgakova <olga.bulgakova.1997@mail.ru>
2023-06-13 16:38:20 +0700

    Ready 'six-card'

--- script.js
+++ script.js
@@ -37,3 +37,50 @@ buttonProfile.addEventListener('click', openedPopup);
 buttonClose.addEventListener('click', closePopup);
 formElement.addEventListener('submit', handleFormSubmit);
 
+// Добавление шести карточек
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
+    alt: 'Зелёные горы на фоне голубого неба'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
+    alt: 'Зимняя река, окружённая возвышеными берегами с лесом'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
+    alt: 'Вид на серые хрущёвки'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
+    alt: 'Пик заснеженной горы на фоне серого-рыжеватого неба на заднем плане и земля с проростающей травой на переднем плане'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
+    alt: 'Прямые рельсы железной дороги, уходящие в даль, окружённые зелёным лесом'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
+    alt: 'Высокая скала являющаяся берегом замёрзшего озера байкал на фоне голубого неба'
+  }
+];
+
+const cardTemplate = document.querySelector('#card-template').content;
+const cardsLinks = document.querySelector('.cards__links');
+
+initialCards.forEach(function (element) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+  cardElement.querySelector('.card__caption').textContent = element.name;
+  cardElement.querySelector('.card__image').src = element.link;
+  cardElement.querySelector('.card__image').alt = element.alt;
+
+  cardsLinks.append(cardElement)
+});
