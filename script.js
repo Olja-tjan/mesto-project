39d9ebfc9fc00cf54994e54aa8521690565bea4a
Olga Bulgakova <olga.bulgakova.1997@mail.ru>
2023-06-19 21:04:24 +0700

    Исправление ошибок

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ const cardAddButton = document.querySelector('.profile__add-button');
 const profileEditButtonClose = profileEditPopup.querySelector('.popup__button-close');
 const cardAddButtonClose = cardAddPopup.querySelector('.popup__button-close');
 const cardZoomButtonClose = cardZoomPopup.querySelector('.popup__button-close');
-const profileEditform = profileEditPopup.querySelector('.popup__container-form');
-const cardAddform = cardAddPopup.querySelector('.popup__container-form');
+const profileEditForm = profileEditPopup.querySelector('.popup__container-form');
+const cardAddForm = cardAddPopup.querySelector('.popup__container-form');
 const profileName = document.querySelector('.profile__name');
 const profileDescription = document.querySelector('.profile__description');
 const nameInput = document.querySelector('#name');
@@ -16,45 +16,12 @@ const imgNameInput = document.querySelector('#image-name');
 const imgLinkInput = document.querySelector('#image-link');
 const cardTemplate = document.querySelector('#card-template').content;
 const cardsLinks = document.querySelector('.cards__links');
-const imagesCards = document.querySelectorAll('.card__image');
-const buttonsLike = document.querySelectorAll('.card__like-button');
-const buttonsTrash = document.querySelectorAll('.card__trash-button');
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
-    alt: 'Зелёные горы на фоне голубого неба'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
-    alt: 'Зимняя река, окружённая возвышеными берегами с лесом'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
-    alt: 'Вид на серые хрущёвки'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
-    alt: 'Пик заснеженной горы на фоне серого-рыжеватого неба на заднем плане и земля с проростающей травой на переднем плане'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
-    alt: 'Прямые рельсы железной дороги, уходящие в даль, окружённые зелёным лесом'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
-    alt: 'Высокая скала являющаяся берегом замёрзшего озера байкал на фоне голубого неба'
-  }
-];
+let img = cardZoomPopup.querySelector('.popup__image');
+let caption = cardZoomPopup.querySelector('.popup__caption');
 
 // Работа модальных окон:
 
-function openedPopup(popup) {
+function openPopup(popup) {
   popup.classList.add("popup_opened");
 };
 
@@ -85,7 +52,6 @@ function createCard(txt, img, alt) {
   buttonTrash.addEventListener('click', deletedCard);
   imageCard.addEventListener('click', (evt) => {
     openedPopupImage(evt);
-    openedPopup(cardZoomPopup);
   });
 
   cardElement.querySelector('.card__caption').textContent = txt;
@@ -136,20 +102,19 @@ function deletedCard(evt) {
 // Открытие попапа с картинкой:
 
 function openedPopupImage(evt) {
-  let img = cardZoomPopup.querySelector('.popup__image');
   img.src = evt.currentTarget.src;
   img.alt = evt.currentTarget.alt;
-  let caption = cardZoomPopup.querySelector('.popup__caption');
   caption.textContent = evt.currentTarget.nextElementSibling.textContent;
+  openPopup(cardZoomPopup);
 };
 
 profileEditButton.addEventListener('click', () => {
   nameInput.value = profileName.textContent;
   jobInput.value = profileDescription.textContent;
-  openedPopup(profileEditPopup)
+  openPopup(profileEditPopup)
 });
 cardAddButton.addEventListener('click', () => {
-  openedPopup(cardAddPopup);
+  openPopup(cardAddPopup);
 });
 profileEditButtonClose.addEventListener('click', () => {
   closePopup(profileEditPopup);
@@ -160,17 +125,5 @@ cardAddButtonClose.addEventListener('click', () => {
 cardZoomButtonClose.addEventListener('click', () => {
   closePopup(cardZoomPopup);
 });
-profileEditform.addEventListener('submit', handleFormSubmitProfileEdit);
-cardAddform.addEventListener('submit', handleFormSubmitCardCreate);
-buttonsLike.forEach(btn => {
-  btn.addEventListener('click', likeCard);
-});
-buttonsTrash.forEach(btn => {
-  btn.addEventListener('click', deletedCard);
-});
-imagesCards.forEach(btn => {
-  btn.addEventListener('click', (evt) => {
-    openedPopupImage(evt);
-    openedPopup(cardZoomPopup);
-  });
-});
+profileEditForm.addEventListener('submit', handleFormSubmitProfileEdit);
+cardAddForm.addEventListener('submit', handleFormSubmitCardCreate);
