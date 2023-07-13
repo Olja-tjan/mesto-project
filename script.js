5497e8e3f7918878c28bfbcd6fe9ef2c83be27fd
Olga Bulgakova <olga.bulgakova.1997@mail.ru>
2023-07-13 11:08:08 +0700

    Валидация форм

--- script.js
+++ script.js
@@ -93,7 +93,6 @@ function likeCard(evt) {
 
 // Удаление карточки:
 
-
 function deletedCard(evt) {
   const cardItem = evt.target.closest('.card');
   cardItem.remove();
@@ -127,3 +126,86 @@ cardZoomButtonClose.addEventListener('click', () => {
 });
 profileEditForm.addEventListener('submit', handleFormSubmitProfileEdit);
 cardAddForm.addEventListener('submit', handleFormSubmitCardCreate);
+
+
+// Валидация форм
+
+
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add('popup__input_error');
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add('popup__input-error_active');
+};
+
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove('popup__input_error');
+  errorElement.classList.remove('popup__input-error_active');
+  errorElement.textContent = '';
+};
+
+const checkInputValidity = (formElement, inputElement) => {
+  if (inputElement.validity.patternMismatch) {
+    inputElement.setCustomValidity(inputElement.dataset.errorMessage);
+  } else {
+    inputElement.setCustomValidity("");
+  }
+
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
+
+
+
+// Функция принимает массив полей
+
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  })
+};
+
+// Функция принимает массив полей ввода
+// и элемент кнопки, состояние которой нужно менять
+
+const toggleButtonState = (inputList, buttonElement) => {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add('popup__button-save_inactive');
+  } else {
+    buttonElement.classList.remove('popup__button-save_inactive');
+  }
+};
+
+
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+  const buttonElement = formElement.querySelector('.popup__button-save');
+
+  toggleButtonState(inputList, buttonElement);
+  // сделать так, чтобы в изменении профиля кнопка была изначально активной
+
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', () => {
+      checkInputValidity(formElement, inputElement)
+      toggleButtonState(inputList, buttonElement);
+    });
+  });
+};
+
+
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll('.popup__container-form'));
+
+  formList.forEach((formElement) => {
+    formElement.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+    });
+    setEventListeners(formElement);
+  });
+};
+
+enableValidation();
