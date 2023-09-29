301762d3d2c5a80d3d799b9c6e9cac0f2b48cc74
Olja-tjan <olga.bulgakova.1997@mail.ru>
2023-09-29 21:18:43 +0700

    edit class API
    Co-authored-by: Chachavitsa678 <Chachavitsa678@yandex.ru>

--- src/components/api.js
+++ src/components/api.js
@@ -1,118 +1,4 @@
-// _checkingStatus
-import { checkingStatus } from './utils'
-export { userData, cardsData, editProfile, editAva, postCard, addLike, removeLike, removeCard };
-
-// Загрузка информации о пользователе с сервера
-// getUserInfo
-
-const userData =
-  fetch('https://nomoreparties.co/v1/plus-cohort-28/users/me', {
-    headers: {
-      authorization: 'ef5df4b1-0580-4314-b10f-2fd4a208efc0'
-    }
-  })
-    .then(checkingStatus);
-
-// Загрузка карточек с сервера
-// getInitialCards
-
-const cardsData =
-  fetch('https://nomoreparties.co/v1/plus-cohort-28/cards', {
-    headers: {
-      authorization: 'ef5df4b1-0580-4314-b10f-2fd4a208efc0'
-    }
-  })
-    .then(checkingStatus);
-
-// Редактирование профиля
-
-function editProfile(nameProfile, aboutProfile) {
-  return fetch('https://nomoreparties.co/v1/plus-cohort-28/users/me', {
-    method: 'PATCH',
-    headers: {
-      authorization: 'ef5df4b1-0580-4314-b10f-2fd4a208efc0',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      name: nameProfile,
-      about: aboutProfile
-    })
-  })
-    .then(checkingStatus);
-};
-
-// Редактирование аватара
-
-function editAva(ava) {
-  return fetch('https://nomoreparties.co/v1/plus-cohort-28/users/me/avatar', {
-    method: 'PATCH',
-    headers: {
-      authorization: 'ef5df4b1-0580-4314-b10f-2fd4a208efc0',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      avatar: ava
-    })
-  })
-    .then(checkingStatus);
-};
-
-// Добавление новой карточки
-
-function postCard(nameCard, linkCard) {
-  return fetch('https://nomoreparties.co/v1/plus-cohort-28/cards', {
-    method: 'POST',
-    headers: {
-      authorization: 'ef5df4b1-0580-4314-b10f-2fd4a208efc0',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      name: nameCard,
-      link: linkCard
-    })
-  })
-    .then(checkingStatus);
-};
-
-// Добавление лайка карточки
-
-function addLike(cardId) {
-  return fetch(`https://nomoreparties.co/v1/plus-cohort-28/cards/likes/${cardId}`, {
-    method: 'PUT',
-    headers: {
-      authorization: 'ef5df4b1-0580-4314-b10f-2fd4a208efc0'
-    }
-  })
-    .then(checkingStatus);
-};
-
-// Удаление лайка карточки
-
-function removeLike(cardId) {
-  return fetch(`https://nomoreparties.co/v1/plus-cohort-28/cards/likes/${cardId}`, {
-    method: 'DELETE',
-    headers: {
-      authorization: 'ef5df4b1-0580-4314-b10f-2fd4a208efc0'
-    }
-  })
-    .then(checkingStatus);
-};
-
-// Удаление карточки
-
-function removeCard(cardId) {
-  return fetch(`https://nomoreparties.co/v1/plus-cohort-28/cards/${cardId}`, {
-    method: 'DELETE',
-    headers: {
-      authorization: 'ef5df4b1-0580-4314-b10f-2fd4a208efc0'
-    }
-  })
-    .then(checkingStatus);
-};
-
-// класс Api
-
-class Api {
+export default class Api {
   constructor({ baseUrl, headers }) {
     this.baseUrl = baseUrl;
     this.headers = headers;
@@ -125,12 +11,18 @@ class Api {
     return Promise.reject(`Ошибка: ${res.status}`);
   }
 
+
+  // Загрузка информации о пользователе с сервера
+
   getUserInfo() {
     return fetch(`${this.baseUrl}/users/me`, {
       headers: this.headers
     })
       .then(this._checkingStatus)
   }
+
+
+  // Загрузка карточек с сервера
 
   getInitialCards() {
     return fetch(`${this.baseUrl}/cards`, {
@@ -139,18 +31,84 @@ class Api {
       .then(this._checkingStatus)
   }
 
-  editUserInfo() {}
 
-  editAvatar() {}
+  // Редактирование профиля
 
-  addCard() {}
+  editUserInfo(nameProfile, aboutProfile) {
+    return fetch(`${this.baseUrl}/users/me`, {
+      method: 'PATCH',
+      headers: this.headers,
+      body: JSON.stringify({
+        name: nameProfile,
+        about: aboutProfile
+      })
+    })
+      .then(this._checkingStatus);
+  }
 
-  removeCard() {}
 
-  addLike() {}
+  // Редактирование аватара
 
-  removeLike() {}
+  editAvatar(ava) {
+    return fetch(`${this.baseUrl}/users/me/avatar`, {
+      method: 'PATCH',
+      headers: this.headers,
+      body: JSON.stringify({
+        avatar: ava
+      })
+    })
+      .then(this._checkingStatus)
+  }
+
+
+  // Добавление новой карточки
+
+  addCard(nameCard, linkCard) {
+    return fetch(`${this.baseUrl}/cards`, {
+      method: 'POST',
+      headers: this.headers,
+      body: JSON.stringify({
+        name: nameCard,
+        link: linkCard
+      })
+    })
+      .then(this._checkingStatus);
+  }
+
+
+  // Удаление карточки
+
+  removeCard(cardId) {
+    return fetch(`${this.baseUrl}/cards/${cardId}`, {
+      method: 'DELETE',
+      headers: this.headers,
+    })
+      .then(this._checkingStatus);
+  }
+
+
+  // Добавление лайка карточки
+
+  addLike(cardId) {
+    return fetch(`${this.baseUrl}/cards/likes/${cardId}`, {
+      method: 'PUT',
+      headers: this.headers,
+    })
+      .then(this._checkingStatus);
+  }
+
+
+  // Удаление лайка карточки
+
+  removeLike(cardId) {
+    return fetch(`${this.baseUrl}/cards/likes/${cardId}`, {
+      method: 'DELETE',
+      headers: this.headers,
+    })
+      .then(this._checkingStatus);
+  }
 }
+
 
 
 // в index.js:
