21d915d41b78a0bdc0fdf30566bf40c8fbd74153
Olja-tjan <olga.bulgakova.1997@mail.ru>
2023-10-01 12:33:54 +0700

    create class PopupWithImage

---
+++ src/components/PopupWithImage.js
@@ -0,0 +1,14 @@
+export class PopupWithImage extends Popup {
+    constructor (popupSelector) {
+      super(popupSelector);
+      this._img = this._popup.querySelector('.popup__image');
+      this._caption = this._popup.querySelector('.popup__caption');
+    }
+
+    openPopup (evt) {
+      this._img.src = evt.currentTarget.src;
+      this._img.alt = evt.currentTarget.alt;
+      this._caption.textContent = evt.currentTarget.nextElementSibling.textContent;
+      openPopup(this._popup);
+    }
+}
